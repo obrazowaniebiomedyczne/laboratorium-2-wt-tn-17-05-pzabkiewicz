f025691d87f802f0393708d1c1b527785a8c24e8
pzabkiewicz <pmz1995@gmail.com>
2018-11-20 19:51:52 +0100

    Update check.py
    odkomentowany plik check.py. Do odczytu i zapisu użyto metod z opencv

--- check.py
+++ check.py
@@ -1,26 +1,24 @@
 from solution import *
+import cv2
 from obpng import read_png, write_png
 
-"""
+
 print("- Ocena dostateczna")
 renew_pictures()
-"""
 
-"""
 print("- Ocena dobra")
-read_png("figures/crushed.png")
+image = cv2.imread("figures/crushed.png", 0)
 erosion = own_simple_erosion(image)
-write_png("results/own_simple_erosion.png")
-"""
+cv2.imwrite("results/own_simple_erosion.png", erosion)
 
-"""
+
 print("- Ocena bardzo dobra")
-read_png("figures/crushed.png")
+image = cv2.imread("figures/crushed.png", 0)
 kernel = np.array([[0, 1, 1, 1, 0],
                    [0, 1, 1, 1, 0],
                    [1, 1, 1, 1, 1],
                    [0, 1, 1, 1, 0],
                    [0, 1, 1, 1, 0]])
 erosion = own_erosion(image, kernel)
-write_png("results/own_erosion.png")
-"""
+cv2.imwrite("results/own_erosion.png", erosion)
+
